df3b12745c13df522f5fd79bd584b48490e694d0
santosh <mishrasantosh2105@gmail.com>
2021-10-22 20:36:41 +0530

    News website class based component

---
+++ src/components/NewsItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date ,source} = this.props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <img src={imageUrl ? imageUrl : "https://images.financialexpress.com/2021/10/StockReuters.jpg"} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}... <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left: '80%',zIndex:'1'}}>
+                            {source}
+                        </span></h5>
+                        <p className="card-text">{description}...</p>
+                        <p className="card-text"><small className="text-muted">by {author ? author : "unknown"} on {new Date(date).toGMTString()} </small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
